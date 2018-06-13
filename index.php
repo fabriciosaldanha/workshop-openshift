b53b42e19e82bafed2ba99dacaf2f497e7b2eb18
fabriciosaldanha <fabricio_saldanha@hotmail.com>
2018-06-13 12:04:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-echo "<h1>Openshift Workshop v3.0</h1> ";
+echo "<h1>Openshift Workshop v4.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "admin", "redhat@123", "workshop");
+$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
