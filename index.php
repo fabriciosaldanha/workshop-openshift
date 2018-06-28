f88f5eaf2362498cf9d327afadbc78cf861e19e0
fabriciosaldanha <fabricio_saldanha@hotmail.com>
2018-06-28 00:56:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "<h1>Openshift Workshop v6.2</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 $data = getdate();
-echo $data[hours]:$data[minutes]:$data[seconds]
+echo $data[hours], $data[minutes], $data[seconds]
